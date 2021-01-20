90dbd768c8e74807d1a96b9c0f74f65710e0d981
Raramuri <zhatab.saifi@zopsmart.com>
2021-01-20 09:41:10 +0530

    Dictionary testing

---
+++ src/main/test/DictionaryTest.java
@@ -0,0 +1,42 @@
+package main.test;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DictionaryTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void insert() {
+    }
+
+    @Test
+    void search() {
+    }
+
+    @Test
+    void meaning() {
+    }
+
+    @Test
+    void correctWord() {
+    }
+
+    @Test
+    void addInList() {
+    }
+
+    @Test
+    void matchWord() {
+    }
+}
