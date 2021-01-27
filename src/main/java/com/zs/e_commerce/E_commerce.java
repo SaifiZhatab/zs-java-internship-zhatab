07d4bd995c5eb85caa692337e7aeb1d4a6a7dce2
Raramuri <zhatab.saifi@zopsmart.com>
2021-01-27 17:06:33 +0530

    Zhatab | E-Commerce | fix all error

--- src/main/java/com/zs/e_commerce/E_commerce.java
+++ src/main/java/com/zs/e_commerce/E_commerce.java
@@ -1,4 +1,4 @@
-package main.java.com.zs.e_commerce;
+package com.zs.e_commerce;
 
 import java.io.FileInputStream;
 import java.sql.*;
@@ -15,38 +15,6 @@ import java.util.logging.Logger;
 public class E_commerce {
     static Connection con;
     private static Logger logger;
-    static Scanner in = new Scanner(System.in);
-
-    /**
-     * This is all prepare statement used in this program
-     */
-    public static PreparedStatement insertInUser, insertInProduct ,printUsers,printProducts ,
-            updateUserData, updateProductData,searchInUer,searchInProduct,deleteUser,deleteProduct,
-            book_product, lastXDayOrder,topXOrder;
-
-    /**
-     * This function initialize the prepareStatement
-     * prepareStatement compile one time and run any time
-     */
-    public static void initialize() throws SQLException {
-
-        insertInProduct = con.prepareStatement("insert into product values (?,?,?,?)");
-        insertInUser = con.prepareStatement("insert into users values (?,?,?,?,?)");
-        printUsers = con.prepareStatement("select * from users");
-        printProducts = con.prepareStatement("select * from product");
-        updateUserData = con.prepareStatement("update users set fisrtname=? , lastname = ?,mobile=?,address=? where id = ?");
-        updateProductData = con.prepareStatement("update product set name=?, quantity=?,price=? where id=?");
-        searchInUer = con.prepareStatement("select * from users where id = ?");
-        searchInProduct = con.prepareStatement("select * from product where id = ?");
-        deleteUser = con.prepareStatement("delete from users where id = ?");
-        deleteProduct = con.prepareStatement("delete from product where id = ?");
-        book_product = con.prepareStatement("insert into orders values (?,?,?,?,?,?,?)");
-        lastXDayOrder = con.prepareStatement("select * from order where user_id = ? order by order_date desc limit ?");
-        topXOrder = con.prepareStatement("select * from orders where user_id = ? order by item desc limit ?");
-
-        logger.info("Successfully create all queries");
-    }
-
 
     /**
      * This class help you to insert user data in Users table
@@ -59,11 +27,13 @@ public class E_commerce {
      *                              when you aren't connect to database and try to insert
      *                              when you insert duplicate id
      */
-    public static void insertInUsers(int id,String firstName,String lastName,
+    static void insertInUsers(int id,String firstName,String lastName,
                                      String mobile,String address) throws SQLException {
 
+        PreparedStatement  insertInUser = con.prepareStatement("insert into users values (?,?,?,?,?)");
+
         if(searchInUser(id)){
-            System.out.println("Key already present in user table ");
+            logger.info("Key already present in user table ");
             return ;
         }
         insertInUser.setInt(1,id);
@@ -72,13 +42,14 @@ public class E_commerce {
         insertInUser.setString(4,mobile);
         insertInUser.setString(5,address);
 
-        int check = insertInUser.executeUpdate();
-
-        if(check == 0){
-            logger.warning("Some internal error comes.Please insert again");
-        }else{
+        try{
+            insertInUser.executeUpdate();
             logger.info("Successfully insert data in User table");
+        }catch (SQLException ex) {
+            logger.warning("Some internal error comes.Please insert again");
         }
+
+        insertInUser.close();
     }
 
     /**
@@ -91,19 +62,21 @@ public class E_commerce {
      *                                  when you aren't connect to database and try to insert
      *                                  when you insert duplicate id
      */
-    public static void insertInProduct(int id,String name,int quantity,int cost) throws SQLException {
+    static void insertInProduct(int id,String name,int quantity,int cost) throws SQLException {
+        PreparedStatement insertInProduct = con.prepareStatement("insert into product values (?,?,?,?)");
+
         insertInProduct.setInt(1,id);
         insertInProduct.setString(2,name);
         insertInProduct.setInt(3,quantity);
         insertInProduct.setInt(4,cost);
 
-        int check = insertInProduct.executeUpdate();
-
-        if(check == 0){
-            logger.warning("Some internal error comes.Please insert again");
-        }else{
+        try{
+            insertInProduct.executeUpdate();
             logger.info("Successfully insert data in Product table");
+        }catch (SQLException ex) {
+            logger.warning("Some internal error comes.Please insert again");
         }
+        insertInProduct.close();
     }
 
     /**
@@ -115,10 +88,13 @@ public class E_commerce {
      * @throws SQLException exception:
      *                      when you're not connect to database and try to find the key
      */
-    public static boolean searchInUser(int id) throws SQLException {
+    static boolean searchInUser(int id) throws SQLException {
+        PreparedStatement searchInUer = con.prepareStatement("select * from users where id = ?");
+
         searchInUer.setInt(1,id);
         ResultSet rs = searchInUer.executeQuery();
 
+        searchInUer.close();
         if(rs.next()){
             return true;
         }else{
@@ -135,15 +111,19 @@ public class E_commerce {
      * @throws SQLException exception:
      *                      when you're not connect to database and try to find the key
      */
-    public static boolean searchInProduct(int id) throws SQLException {
+    static boolean searchInProduct(int id) throws SQLException {
+        PreparedStatement searchInProduct = con.prepareStatement("select * from product where id = ?");
+
         searchInProduct.setInt(1,id);
         ResultSet rs = searchInProduct.executeQuery();
+        searchInProduct.close();
 
         if(rs.next()){
             return true;
         }else{
             return false;
         }
+
     }
 
     /**
@@ -151,25 +131,21 @@ public class E_commerce {
      * @throws SQLException  Exception :
      *                         when you aren't connect to database and try to fetch data
      */
-    public static void printUsers() throws SQLException {
-        System.out.println("This is users database table");
+    static void printUsers() throws SQLException {
+        PreparedStatement printUsers = con.prepareStatement("select * from users");
+
+        logger.info("This is users database table");
         ResultSet  rs = printUsers.executeQuery();
 
         logger.info ("User id  :  firstName : LastName  : Mobile   :  Address");
 
-        Set<String> set = new HashSet<String>();
 
         while(rs.next()){
-            String details = (rs.getInt(1)+ " : " +rs.getString(2) + " : " + rs.getString(3) +  " : "
-                    + rs.getString(4) +  " : " + rs.getString(5));
-
-            set.add(details);
+            logger.info(rs.getInt("user_id")+ " : " +rs.getString("fisrtName") + " : "
+                    + rs.getString("lastName") +  " : " + rs.getString("mobile") +  " : "
+                    + rs.getString("address"));
         }
-        Iterator<String > iterator = set.iterator();
-
-        while(iterator.hasNext()){
-            System.out.println(iterator.next());
-        }
+        printUsers.close();
     }
 
 
@@ -178,21 +154,17 @@ public class E_commerce {
      * @throws SQLException  Exception :
      *                         when you aren't connect to database and try to fetch data
      */
-    public static void printProducts() throws SQLException {
+    static void printProducts() throws SQLException {
+        PreparedStatement printProducts = con.prepareStatement("select * from product");
         ResultSet rs = printProducts.executeQuery();
         logger.info("Product id  :   name  :  quantity  : price ");
 
-        Set<String> set = new HashSet<String>();
+
         while (rs.next()){
-
-            String details = (rs.getInt(1) + " : "+rs.getString(2) + " : " + rs.getInt(3) +" : " +rs.getInt(4));
-            set.add(details);
+            logger.info (rs.getInt("product_id") + " : "+rs.getString("name") + " : "
+                    + rs.getInt("quantiy") +" : " +rs.getInt("price"));
         }
-
-        Iterator<String > iterator = set.iterator();
-        while(iterator.hasNext()){
-            System.out.println(iterator.next());
-        }
+        printProducts.close();
     }
 
     /**
@@ -206,25 +178,23 @@ public class E_commerce {
      *      *                      when you're not connect with database and try to update
      *      *                      or if the id key is not present in table
      */
-    public static void updateUserData(int id,String firstName,String lastName,
+    static void updateUserData(int id,String firstName,String lastName,
                                       String mobile,String address) throws SQLException {
+        PreparedStatement  updateUserData = con.prepareStatement("update users set fisrtname=? , lastname = ?,mobile=?,address=? where id = ?");
+
         updateUserData.setString(1,firstName);
         updateUserData.setString(2,lastName);
         updateUserData.setString(3,mobile);
         updateUserData.setString(4,address);
         updateUserData.setInt(5,id);
 
-        int check = updateUserData.executeUpdate();
-
-        /**
-         * when executeUpdate is return 0, that means row doesn't enter in it.
-         * when executeUpdate is return 1, that means row is successfully insert in table
-         */
-        if(check == 0){
-            logger.warning("Some internal error comes.Please update again");
-        }else{
-            logger.info("Successfully update data in User table");
-        }
+       try {
+           updateUserData.executeUpdate();
+           logger.info("Successfully update data in User table");
+       }catch (SQLException ex) {
+           logger.warning("Some internal error comes.Please update again");
+       }
+       updateUserData.close();
     }
 
     /**
@@ -237,24 +207,22 @@ public class E_commerce {
      *                      when you're not connect with database and try to update
      *                      or if the id key is not present in table
      */
-    public static void updateProductData(int id,String name,int quantity,int cost) throws SQLException {
+    static void updateProductData(int id,String name,int quantity,int cost) throws SQLException {
+        PreparedStatement updateProductData = con.prepareStatement("update product set name=?, quantity=?,price=? where id=?");
+
         updateProductData.setString(1,name);
         updateProductData.setInt(2,quantity);
         updateProductData.setInt(3,cost);
 
         updateProductData.setInt(4,id);
 
-        int check = updateProductData.executeUpdate();
-
-        /**
-         * when executeUpdate is return 0, that means row doesn't enter in it.
-         * when executeUpdate is return 1, that means row is successfully insert in table
-         */
-        if(check == 0){
-            logger.warning("Some internal error comes.Please update again");
-        }else{
-            logger.info("Successfully update data in Product table");
-        }
+       try{
+           updateProductData.executeUpdate();
+           logger.info("Successfully update data in Product table");
+       }catch (SQLException ex) {
+           logger.warning("Some internal error comes.Please update again");
+       }
+       updateProductData.close();
     }
 
     /**
@@ -263,27 +231,25 @@ public class E_commerce {
      * @throws SQLException   exception
      *                        when you're not connect to database and try to delete the key
      */
-    public static void deleteUser(int id) throws SQLException {
+    static void deleteUser(int id) throws SQLException {
+        PreparedStatement deleteUser = con.prepareStatement("delete from users where id = ?");
+
+        /**
+         * check key present in table or not after that delete
+         */
         if(!searchInUser(id)){
-            /**
-             * check key present in table or not after that delete
-             */
             logger.warning("Key not present in table");
             return ;
         }
 
         deleteUser.setInt(1,id);
-        int check = deleteUser.executeUpdate();
-
-        /**
-         * when executeUpdate is return 0, that means row doesn't enter in it.
-         * when executeUpdate is return 1, that means row is successfully insert in table
-         */
-        if(check == 0){
-            logger.warning("Some internal error comes.Please try again");
-        }else{
+        try{
+            deleteUser.executeUpdate();
             logger.info("Successfully delete "+id +" in User table");
+        }catch (SQLException ex) {
+            logger.warning("Some internal error comes.Please try again");
         }
+       deleteUser.close();
     }
 
     /**
@@ -292,27 +258,25 @@ public class E_commerce {
      * @throws SQLException   exception
      *                        when you're not connect to database and try to delete the key
      */
-    public static void deleteProduct(int id) throws SQLException {
+    static void deleteProduct(int id) throws SQLException {
+        PreparedStatement deleteProduct = con.prepareStatement("delete from product where id = ?");
+
+        /**
+         * if key is not present in table
+         */
         if(!searchInProduct(id)){
-            /**
-             * if key is not present in table
-             */
-            System.out.println("Key isn't present in product table");
+            logger.info("Key isn't present in product table");
             return;
         }
 
         deleteProduct.setInt(1,id);
-        int check = deleteProduct.executeUpdate();
-
-        /**
-         * when executeUpdate is return 0, that means row doesn't enter in it.
-         * when executeUpdate is return 1, that means row is successfully insert in table
-         */
-        if(check == 0){
-            logger.warning("Some internal error comes.Please try again");
-        }else{
+        try{
+            deleteProduct.executeUpdate();
             logger.info("Successfully delete "+id +" in User table");
+        }catch (SQLException ex) {
+            logger.warning("Some internal error comes.Please try again");
         }
+        deleteProduct.close();
     }
 
 
@@ -327,8 +291,10 @@ public class E_commerce {
      * @param item          number of itme purchase
      * @throws SQLException Exception
      */
-    public static void book_product(int order_id,int product_id,int user_id,String payment,
+    static void book_product(int order_id,int product_id,int user_id,String payment,
                                     String order_date,String dilivery_date, int item) throws SQLException {
+       PreparedStatement book_product = con.prepareStatement("insert into orders values (?,?,?,?,?,?,?)");
+
         book_product.setInt(1,order_id);
         book_product.setInt(2,product_id);
         book_product.setInt(3,user_id);
@@ -337,17 +303,13 @@ public class E_commerce {
         book_product.setString(6,dilivery_date);
         book_product.setInt(7,item);
 
-        int check = book_product.executeUpdate();
-
-        /**
-         * when executeUpdate is return 0, that means row doesn't enter in it.
-         * when executeUpdate is return 1, that means row is successfully insert in table
-         */
-        if(check == 0){
-            logger.warning("Some internal error comes.Please try again");
-        }else{
-            logger.info("Successfully order the product in User table");
-        }
+       try{
+           book_product.executeUpdate();
+           logger.info("Successfully order the product in User table");
+       }catch (SQLException ex) {
+           logger.warning("Some internal error comes.Please try again");
+       }
+       book_product.close();
     }
 
     /**
@@ -357,15 +319,19 @@ public class E_commerce {
      * @throws SQLException Exception
      *                      when you're not connect with database and try to access the database
      */
-    public static void lastXDayOrder(int user_id,int days) throws SQLException {
+    static void lastXDayOrder(int user_id,int days) throws SQLException {
+        PreparedStatement  lastXDayOrder = con.prepareStatement("select * from order where user_id = ? order by order_date desc limit ?");
+
         lastXDayOrder.setInt(1,user_id);
         lastXDayOrder.setInt(2,days);
         ResultSet rs = lastXDayOrder.executeQuery();
 
         while(rs.next()){
-            logger.info(rs.getInt(1) + " "+rs.getInt(2) +" "+ rs.getString(4) +
-                    " " + rs.getString(5) +" " +rs.getString(6) );
+            logger.info(rs.getInt("product_id") + " " + rs.getInt("user_id") + " "
+                    + rs.getString("payment") + " " + rs.getString("order_date") + " "
+                    + rs.getString("dilivery_date"));
         }
+        lastXDayOrder.close();
     }
 
     /**
@@ -374,7 +340,9 @@ public class E_commerce {
      * @param top   top order
      * @throws SQLException exception
      */
-    public static void topXOrder(int user_id,int top) throws SQLException {
+    static void topXOrder(int user_id,int top) throws SQLException {
+        PreparedStatement topXOrder = con.prepareStatement("select * from orders where user_id = ? order by item desc limit ?");
+
         topXOrder.setInt(1,user_id);
         topXOrder.setInt(2,top);
 
@@ -384,12 +352,10 @@ public class E_commerce {
         ResultSet rs = topXOrder.executeQuery();
 
         while(rs.next()){
-            /**
-             * loop run until you fetch all row present in ResultSet
-             */
-            logger.info(rs.getInt(1) + " " + rs.getInt(2) + " " + rs.getString(4) + " " +
-                    rs.getString(5) +" "+ rs.getString(6) + " "+rs.getInt(7));
+            logger.info(rs.getInt("product_id") + " " + rs.getString("payment") + " "
+            + rs.getString("order_date"));
         }
+        topXOrder.close();
     }
 
     /**
@@ -399,6 +365,7 @@ public class E_commerce {
      * @param args
      */
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
         try {
             /**
              * make the connection between java program and postgres database
@@ -409,7 +376,6 @@ public class E_commerce {
 
             logger.info("Connection make successfully");
 
-            initialize();
 
             /**
              * initialize logger
@@ -421,16 +387,16 @@ public class E_commerce {
             /**
              * try to insert in product table using java
              */
-            System.out.print("Enter product id = ");
+            logger.info("Enter product id = ");
             int id = in.nextInt();
 
-            System.out.print("Enter product name = ");
+            logger.info("Enter product name = ");
             String name = in.next();
 
-            System.out.print("Enter the quantity = ");
+            logger.info("Enter the quantity = ");
             int quantity = in.nextInt();
 
-            System.out.print("Enter the price = ");
+            logger.info("Enter the price = ");
             int price = in.nextInt();
 
             insertInProduct(id,name,quantity,price);
